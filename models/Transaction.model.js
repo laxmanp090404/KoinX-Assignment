e6608fbc51153a76e43ff9e8f4e95cc48b0dd9e3
laxmanp090404 <laxman.p2022cse@sece.ac.in>
2024-08-31 23:26:13 +0530

    feat: implement MongoDB schema to store user transactions

---
+++ models/Transaction.model.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const transactionSchema = new mongoose.Schema({
+  address: {
+    type: String,
+    required: true,
+  },
+  transactions: [
+    {
+      blockNumber: String,
+      timeStamp: String,
+      hash: String,
+      nonce: String,
+      blockHash: String,
+      transactionIndex: String,
+      from: String,
+      to: String,
+      value: String,
+      gas: String,
+      gasPrice: String,
+      isError: String,
+      txreceipt_status: String,
+      input: String,
+      contractAddress: String,
+      cumulativeGasUsed: String,
+      gasUsed: String,
+      confirmations: String,
+    }
+  ],
+}, { timestamps: true });
+
+module.exports = mongoose.model('Transaction', transactionSchema);
